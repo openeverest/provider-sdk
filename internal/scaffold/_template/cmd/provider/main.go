a36194b695edbf5891dde22ef237980d5267f5b2
Chi Fujii <chi.fujii@solanica.io>
2026-06-15 11:58:59 +0900

    update helm values deployments and pass param to main
    Signed-off-by: Chi Fujii <chi.fujii@solanica.io>

--- internal/scaffold/_template/cmd/provider/main.go
+++ internal/scaffold/_template/cmd/provider/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -13,18 +14,31 @@ import (
 
 // main is the entry point for the provider.
 func main() {
+	var serverPort int
+	var metricsBindAddress string
+
+	flag.IntVar(&serverPort, "server-port", 8082, "The port for the provider HTTP server.")
+	flag.StringVar(&metricsBindAddress, "metrics-bind-address", "8081", "The address the metrics endpoint binds to. Use 0 to disable.")
+	flag.Parse()
+
 	l := ctrl.Log.WithName("setup")
 	ctx := ctrl.SetupSignalHandler()
 
 	p := provider.New()
 
-	r, err := reconciler.New(ctx, p,
-		// Enable HTTP server for validation and schema endpoints.
+	opts := []reconciler.ReconcilerOption{
+		// Enable HTTP server for validation endpoint
 		reconciler.WithServer(reconciler.ServerConfig{
-			Port:           8082,
+			Port:           serverPort,
 			ValidationPath: "/validate",
 		}),
-	)
+	}
+
+	if metricsBindAddress != "0" {
+		opts = append(opts, reconciler.WithMetrics(metricsBindAddress))
+	}
+
+	r, err := reconciler.New(ctx, provider, opts...)
 	if err != nil {
 		l.Error(err, "unable to create reconciler")
 		os.Exit(1)
